d7c0a024c8f6ffcefc0be92ed9dd5b4c4f412bc0
remi-code <ananyakapil13@gmail.com>
2026-03-12 17:10:38 +0530

    first commit

--- java.js
+++ java.js
@@ -145,12 +145,3 @@ audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 
 });
 
-
-
-ScrollReveal().reveal('.about p', {
-    distance: '50px',
-    duration: 1000,
-    easing: 'ease-in-out',
-    origin: 'bottom',
-    interval: 200
-});
